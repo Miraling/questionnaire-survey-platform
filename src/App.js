ec8da559b9650662a92cd11e9c0f8c9e31029510
Miraling <lanbailing.ok@163.com>
2021-08-08 21:05:29 +0800

    add_exact_in_router

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         return (
             <ConfigProvider locale={cn}>
                 <BrowserRouter>
-                    <Route path={'/'} component={Login}/>
+                    <Route exact path={'/'} component={Login}/>
                     <Route path={'/fillquestionnaire'} component={FillQuestionnaire}/>
                     <Route path={'/fillquestionnairesubmitcomplete'} component={QuestionnaireSubmitComplete}/>
                     <Route path={'/dataanalysis'} component={DataAnalysis}/>
